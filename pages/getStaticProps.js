79db8772c3be329454c3aeb973e6c7e069c3e56d
arsalan manzoor <arsalan1998m@gmail.com>
2021-10-21 23:17:51 +0500

    Learning static site generation

---
+++ pages/getStaticProps.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+
+function HomePage(props) {
+  console.log('props=>', props);
+  return (
+    <ul>
+      {props.products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+
+  const jsonData = await fs.promises.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
+
+export default HomePage;
