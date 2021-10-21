85f9baf30186493a7b44c4bdd6a56cb2192cd1df
arsalan manzoor <arsalan1998m@gmail.com>
2021-10-21 23:56:51 +0500

    more options of getStaticProps, notFound property and redirect property

--- pages/index.js
+++ pages/index.js
@@ -9,27 +9,33 @@ function HomePage(props) {
   );
 }
 
-// What if you have data that changes fruquently?
-// IF WE ADD 4th product after page was deployed then we have to rebuild and redeploy the page all the time and that doesn't sound like a great thing to do.
+// More Options NextJS offers to us and we need them
+// 1. we can see that getStaticProps recieves an argument we just haven't used it this far. But we do actually get an object here as our  argument as a parameter with some extra information about this page when it's executed by next.js for example we would get any dynamic params any dynamic path segment values which we'll see in next ignore it now. and we also get a couple of other pieces of information which at the moment though, don't matter to us.
 
-// NextJS also has solution for this
+// Now lets close look at getStaticProps return object props and revalidate now there are two other keys, which we can set on this object one key is the notFound key which wants a boolean value which is either true or false if we set this to true this page will return a 404 error not found page instead of normal page now why you do this
 
-// ISR(Incremental Static Generation) it means you are not generate statically page at build time but that it's continously updated even after deployment without you re-deploying it
+// Well, if the code here where we fetching data fails to fetch data for whatever reason then we could
 
-// In ISR NextJS generates new pages & re-generates current pages on the fly when data is updated
+// second key is redirect key , this allows you to redirect the user, instead of rendering the page content, we can redirect to another page
 
-// so you pre-generate a page, but then you can also tell NextJs that a given page should be re-generated again for every incoming request at most every X seconds.
-
-// we can use revalidate property with timer value where we define the time
-
-export async function getStaticProps() {
-  console.log('RE-(GENERATING)');
-
+export async function getStaticProps(context) {
   const resp = await fetch(
     'https://notesapplication-backend.herokuapp.com/notes'
   );
 
   const data = await resp.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (data.length === 0) {
+    return {
+      redirect: '/no-data',
+    };
+  }
 
   return {
     props: {
