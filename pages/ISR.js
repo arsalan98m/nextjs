2d3595ff0cb451d84e7b27f1a8ba56a4a44b8a39
arsalan manzoor <arsalan1998m@gmail.com>
2021-10-21 23:09:24 +0500

    Learning ISR Incremental Static Generation

---
+++ pages/ISR.js
@@ -0,0 +1,42 @@
+function HomePage(props) {
+  console.log(props);
+  return (
+    <ul>
+      {props.notes.map((product) => (
+        <li>{product.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+// What if you have data that changes fruquently?
+// IF WE ADD 4th product after page was deployed then we have to rebuild and redeploy the page all the time and that doesn't sound like a great thing to do.
+
+// NextJS also has solution for this
+
+// ISR(Incremental Static Generation) it means you are not generate statically page at build time but that it's continously updated even after deployment without you re-deploying it
+
+// In ISR NextJS generates new pages & re-generates current pages on the fly when data is updated
+
+// so you pre-generate a page, but then you can also tell NextJs that a given page should be re-generated again for every incoming request at most every X seconds.
+
+// we can use revalidate property
+
+export async function getStaticProps() {
+  console.log('RE-(GENERATING)');
+
+  const resp = await fetch(
+    'https://notesapplication-backend.herokuapp.com/notes'
+  );
+
+  const data = await resp.json();
+
+  return {
+    props: {
+      notes: data.notes,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-function HomePage() {
+function HomePage(props) {
+  console.log('props=>', props);
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      <li>Product 1 11</li>
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  console.log('getStaticprops');
+  return {
+    props: {
+      testing: 'abc',
+    },
+  };
 }
 
 export default HomePage;
