0ff93ef914ca17926c426c37d1dcea347d7484c7
arsalan manzoor <arsalan1998m@gmail.com>
2021-10-22 22:09:26 +0500

    learning dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import fs from 'fs';
+import Link from 'next/link';
+import path from 'path';
 function HomePage(props) {
-  console.log(props);
   return (
     <ul>
-      {props.notes.map((product) => (
-        <li>{product.text}</li>
+      {props.products.map((product) => (
+        <Link href={`/products/${product.id}`} key={product.id}>
+          {product.title}
+        </Link>
       ))}
     </ul>
   );
@@ -12,34 +16,17 @@ function HomePage(props) {
 // More Options NextJS offers to us and we need them
 // 1. we can see that getStaticProps recieves an argument we just haven't used it this far. But we do actually get an object here as our  argument as a parameter with some extra information about this page when it's executed by next.js for example we would get any dynamic params any dynamic path segment values which we'll see in next ignore it now. and we also get a couple of other pieces of information which at the moment though, don't matter to us.
 
-// Now lets close look at getStaticProps return object props and revalidate now there are two other keys, which we can set on this object one key is the notFound key which wants a boolean value which is either true or false if we set this to true this page will return a 404 error not found page instead of normal page now why you do this
-
-// Well, if the code here where we fetching data fails to fetch data for whatever reason then we could
-
-// second key is redirect key , this allows you to redirect the user, instead of rendering the page content, we can redirect to another page
-
 export async function getStaticProps(context) {
-  const resp = await fetch(
-    'https://notesapplication-backend.herokuapp.com/notes'
-  );
+  console.log('context=>', context);
+  console.log('RE-GENERATING');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
 
-  const data = await resp.json();
-
-  if (!data) {
-    return {
-      notFound: true,
-    };
-  }
-
-  if (data.length === 0) {
-    return {
-      redirect: '/no-data',
-    };
-  }
+  const jsonData = await fs.promises.readFile(filePath);
+  const data = JSON.parse(jsonData);
 
   return {
     props: {
-      notes: data.notes,
+      products: data.products,
     },
     revalidate: 10,
   };
---
+++ pages/products/[pid].js
@@ -0,0 +1,68 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  // Agar hum yahan if ki condition nahi lagana chaty tu hum fallback main value ki jaga 'blocking' likh dyngy tu ussy nextjs khud hi wait karega jab tak page wapas nahi ajata
+  if (!loadedProduct) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <div>
+      <h1>{loadedProduct.title}</h1>
+    </div>
+  );
+}
+
+// Dynamic id ky uppar agar hum getStaticProps use karty hain tu yeh hamain pre-generated page nahi bana kar dega reason kun k yeh keh raha hai k mujhe nahi pata tumhari ids kitni hai
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+// goal of getStaticPath is to tell NextJS which instances of this dynamic page should be generated
+
+// The fallback key can help you if you have a lot of pages that would need to-be pre-generated
+
+// image ky hamary pass millions of products hain tu kya hum millions of pages pre-geneate karin gay tu yeh cheez tu hamary liyeh waste of time or waste of resource hai tu iska hall NextJS walon ny fallback ky zairiyeh nikala kasiy dekhty hain..
+
+// In fallback we could decide only-pre-render some pages so let's say we wanna pre-render the page with product id 1 becuase that's a highly frequented page it's visited very often but we don't wanna pre-geneate the other two pages.With fallback set to true, that's possible Now if i click product 2 then it will show product 2 hala ky main neeche paths main params p2 or p3 delete kar diya phir bhi woh mujhe page genreate kar k de raha kun kun k mainy fallback true kardiya hai isWaja sy
+// they are generating this page in just-in-time when a request reaches the server so they are only pre-generated when they are needed
+
+export async function getStaticPaths() {
+  return {
+    // paths: [
+    //   { params: { pid: 'p1' } },
+    //   { params: { pid: 'p2' } },
+    //   { params: { pid: 'p3' } },
+    // ],
+
+    paths: [{ params: { pid: 'p1' } }],
+
+    fallback: true,
+  };
+}
+
+export default ProductDetailPage;
